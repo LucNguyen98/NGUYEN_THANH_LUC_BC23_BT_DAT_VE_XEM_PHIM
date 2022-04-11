3d43998104adb81373a43f48ce375189edd41bb0
Nguyen Thanh Luc <luc.nguyen150198@gmail.com>
2022-04-11 20:20:21 +0700

    fix css

--- src/Components/BaiTapBookingTicket/BaiTapBookingTicket.jsx
+++ src/Components/BaiTapBookingTicket/BaiTapBookingTicket.jsx
@@ -13,30 +13,33 @@ export default class BaiTapBookingTicket extends Component {
 
   render() {
     return (
-      <div
-        style={{
-          backgroundImage: "url(./bgmovie.jpg)",
-          height: '100vh',
-          width: '100%',
-          backgroundSize: '100%',
-          backgroundRepeat: 'no-repeat',
-          position: 'fixed'
-        }}
-      >
+      <div>
+        <div
+          style={{
+            backgroundImage: "url(./bgmovie.jpg)",
+            height: '100%',
+            width: '100%',
+            backgroundSize: '100%',
+            backgroundRepeat: 'no-repeat',
+            position: 'fixed'
+          }}
+        >
+        </div>
         <div className="overlay">
-          <div class="container-fluid">
-            <div class="row">
-              <div className="col-8">
-                <h1 className='display-4' style={{
-                  color: '#ffc107'
-                }}>ĐẶT VÉ XEM PHIM CYBERLEARN.VN</h1>
-                <h3 className='mt-3 text-light'>Màn hình</h3>
-                <div className="screen"></div>
-                {this.renderTicketList()}
-              </div>
-              <div className="col-4">
-                  <GioHangBookingTicket/>
-              </div>
+        </div>
+
+        <div class="container-fluid">
+          <div class="row">
+            <div className="col-8">
+              <h1 className='display-4' style={{
+                color: '#ffc107'
+              }}>ĐẶT VÉ XEM PHIM CYBERLEARN.VN</h1>
+              <h3 className='mt-3 text-light'>Màn hình</h3>
+              <div className="screen"></div>
+              {this.renderTicketList()}
+            </div>
+            <div className="col-4">
+              <GioHangBookingTicket />
             </div>
           </div>
         </div>
